8a96ce5a6e650cc30a423bc2959a83ad4a2a4ab5
BrunoAgst <bruno_agst@hotmail.com>
2020-07-23 15:36:54 -0300

    alteraçao na declaraçao de variavel

--- function/verifyJSON.js
+++ function/verifyJSON.js
@@ -2,17 +2,16 @@ const requiredF = require('./requiredField');
 const notRequiredF = require('./notRequiredField');
 
 module.exports = async function verifyJSON(body){
-    var name, email, phone, store, work, description, facebook, instagram;
     try{
-        name = await requiredF(body.name);
-        email = await requiredF(body.email);
-        phone = await requiredF(body.phone);
-        store = await notRequiredF(body.store, 80);
-        work =  await notRequiredF(body.work, 80);
-        description =  await notRequiredF(body.description, 250);
-        address = await notRequiredF(body.address, 100);
-        facebook = await notRequiredF(body.facebook, 50);
-        instagram = await notRequiredF(body.instagram, 50);
+        var name = await requiredF(body.name);
+        var email = await requiredF(body.email);
+        var phone = await requiredF(body.phone);
+        var store = await notRequiredF(body.store, 80);
+        var work =  await notRequiredF(body.work, 80);
+        var description =  await notRequiredF(body.description, 250);
+        var address = await notRequiredF(body.address, 100);
+        var facebook = await notRequiredF(body.facebook, 50);
+        var instagram = await notRequiredF(body.instagram, 50);
 
         if(name == true 
             && email == true 
