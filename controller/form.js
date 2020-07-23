9976b373574b9d1f65da8887bc5b0031867db91b
BrunoAgst <bruno_agst@hotmail.com>
2020-07-23 17:08:25 -0300

    implementando o PUT

--- controller/form.js
+++ controller/form.js
@@ -5,9 +5,9 @@ const verifyJSON = require('../function/verifyJSON');
 
 router.get('/v1/form', (req, res) => {
     
-    database.select().table("form").orderBy("id", "asc").then(data => {
+    database.select().table("form").orderBy("id", "asc").then(response => {
         res.statusCode = 200;
-        res.json(data);
+        res.json(response);
         
     }).catch(err => {
         res.statusCode = 400;
@@ -17,15 +17,15 @@ router.get('/v1/form', (req, res) => {
 });
 
 router.post('/v1/form', async (req, res) => {
-    const form = req.body;
-    var result = await verifyJSON(form);
+    const data = req.body;
+    var result = await verifyJSON(data);
 
     if(result !== true){
         res.sendStatus(400);
         return
     }
 
-    database.insert(form).into("form").then(data => {
+    database.insert(data).into("form").then(response => {
         res.statusCode = 200;
         res.send("Cadastrado com sucesso");
         return
@@ -40,21 +40,21 @@ router.post('/v1/form', async (req, res) => {
 
 router.delete('/v1/form/:id', (req, res) => {
     const id = req.params.id;
-    var idN = parseInt(id);
+    const idN = parseInt(id);
 
     if(idN === null || idN === undefined || idN.length <= 0){
         res.statusCode = 400;
         res.send("ID inválido");
         return
     }
-    database.select().where({id: idN}).table("form").then(data => {
+    database.select().where({id: idN}).table("form").then(response => {
         if(data.length < 1){
             res.statusCode = 404;
             res.send("Não encontrado");
             return
         };
 
-        database.where({id: idN}).delete().table("form").then(data => {
+        database.where({id: idN}).delete().table("form").then(response => {
             res.statusCode = 200;
             res.send("Deletado com sucesso");
     
@@ -63,8 +63,51 @@ router.delete('/v1/form/:id', (req, res) => {
             res.send("Erro");
     
         });
+    });     
+});
+
+router.put('/v1/form/:id', (req, res) => {
+    const id = req.params.id;
+    const idN = parseInt(id);
+
+    const data = req.body;
+
+    if(idN === null || idN === undefined || idN.length <= 0){
+        res.statusCode = 400;
+        res.send("ID inválido");
+        return
+    }
+
+    database.select().where({id: idN}).table("form").then(response => {
+        if(response.length < 1){
+            res.statusCode = 404;
+            res.send("Não encontrado");
+            return
+        };
+
+        database.where({id: idN}).update({
+            name: data.name,
+            email: data.email,
+            phone: data.phone,
+            store: data.store,
+            work: data.work,
+            description: data.description,
+            address: data.address,
+            facebook: data.facebook,
+            instagram: data.instagram
+        }).table("form").then(response => {
+            res.statusCode = 200;
+            res.send("Alterado com sucesso");
+    
+        }).catch(err => {
+            res.statusCode = 404;
+            res.send("Erro");
+        });
     });
-        
+
+
+
+
 });
 
 module.exports = router;
