45ec5eb0a790b9c7de697a3e430eb49669ab96b3
BrunoAgst <bruno_agst@hotmail.com>
2020-07-25 17:29:13 -0300

    estrutura da rota de autenticaçao

--- controller/auth.js
+++ controller/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const verifyLogin = require('../function/verifyLogin');
 const database = require('../database/database');
+const bcrypt = require('bcrypt');
 
 router.post('/v1/auth', async (req, res) => {
 
@@ -14,6 +15,35 @@ router.post('/v1/auth', async (req, res) => {
         res.json(data);
         return
     }
+
+    database.select(["email", "password"]).table('users').then(data => {
+
+        for(var i = 0; i <= data.length; i++){
+            if(email == data[i].email){
+                var pass = bcrypt.compareSync(password, data[i].password);
+
+                if(pass === true){
+                    res.json("Token");
+                    return
+                }
+                else{
+                    res.statusCode = 400;
+                    res.json("Senha incorreta");
+                    return  
+                }
+            }
+           
+            if(i <= parseInt(data.length)){
+                res.statusCode = 400;
+                res.json("Email não encontrado");
+                return
+            }                     
+        }
+        
+    }).catch(err => {
+        res.statusCode = 400;
+        res.json("Erro");
+    });
 
 });
 
