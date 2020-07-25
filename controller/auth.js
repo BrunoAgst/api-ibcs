cb6400cb14c66ce9f70671009b1117e05b73bd9d
BrunoAgst <bruno_agst@hotmail.com>
2020-07-25 14:58:04 -0300

    tratamento de erro

--- controller/auth.js
+++ controller/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const verifyLogin = require('../function/verifyLogin');
+const database = require('../database/database');
 
 router.post('/v1/auth', async (req, res) => {
 
-    const {email, password} = req.body; 
+    const {email, password} = req.body;
+    
+    const data = await verifyLogin(email, password);
+
+    if(data != undefined){
+        res.statusCode = 400;
+        res.json(data);
+        return
+    }
 
 });
+
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 const formController = require('./controller/form');
+const authController = require('./controller/auth');
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(formController);
+app.use(authController);
 
 app.listen(3000, () => {
     console.log("server running");
